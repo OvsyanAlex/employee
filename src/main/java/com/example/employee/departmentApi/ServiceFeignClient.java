f49de008740d7b13ec4917f9218c25abb48a5378
Alexey Ovsyannikov <aleksei_ovsiannikov1@epam.com>
2022-05-12 23:52:45 +0400

    change Fallback Class neme

--- src/main/java/com/example/employee/departmentApi/ServiceFeignClient.java
+++ src/main/java/com/example/employee/departmentApi/ServiceFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "department", url = "http://localhost:8082", fallback = StatisticFallbackFactory.class)
+@FeignClient(name = "department", url = "http://localhost:8082", fallback = FallbackClass.class)
 
 public interface ServiceFeignClient {
 
